0a2e16571cb8789a7425f14d784825e107fb7dba
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-09-06 18:33:07 +0530

    Update Swapping.java

--- Day-3 Bit Manipulation/Assignments/Swapping.java
+++ Day-3 Bit Manipulation/Assignments/Swapping.java
@@ -1,1 +1,12 @@
-Class
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt(),B=sc.nextInt();
+        System.out.println("Before Swapping : "+A+" "+B);
+        A=A^B;
+        B=A^B;
+        A=A^B;
+        System.out.println("After Swapping : "+A+" "+B);
+    }
+}
