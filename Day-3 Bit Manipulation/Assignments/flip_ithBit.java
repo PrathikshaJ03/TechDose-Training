eab95150389526281415f531c20ed931fe3bc201
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-09-06 19:55:51 +0530

    Create flip_ithBit.java

---
+++ Day-3 Bit Manipulation/Assignments/flip_ithBit.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt(),pos=sc.nextInt();
+		System.out.print(N^(1<<(pos-1)));
+	}
+}
