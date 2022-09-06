02e3cd74939a60c8b8aa454a30b7e361f8005163
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-09-06 20:15:43 +0530

    Update 338.CountingBits.java

--- Day-3 Bit Manipulation/Leetcode Programs/338.CountingBits.java
+++ Day-3 Bit Manipulation/Leetcode Programs/338.CountingBits.java
@@ -1,1 +1,22 @@
-class Solution{
+class Solution {
+    
+    public static int countOfSetBit(int N){
+        int count=0;
+        while(N>0){
+            if((N&1)==1){
+                count++;
+            }
+            N=N>>1;
+        }
+        return count;
+    }
+    
+    public int[] countBits(int n) {
+        int res[]=new int[n+1];
+        res[0]=0;
+        for(int i=1;i<=n;i++){
+            res[i]=countOfSetBit(i);
+        }
+        return res;
+    }
+}
