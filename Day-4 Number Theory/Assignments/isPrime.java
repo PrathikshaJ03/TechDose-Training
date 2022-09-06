359d217d9aaeab5bcfc8040591959ec04a3ae7b3
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-09-06 21:05:19 +0530

    Update isPrime.java

--- Day-4 Number Theory/Assignments/isPrime.java
+++ Day-4 Number Theory/Assignments/isPrime.java
@@ -1,1 +1,23 @@
-class
+import java.util.*;
+class Main {
+    
+    public static boolean isPrime(int N){
+        for(int i=2;i*i<=N;i++){
+            if(N%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        if(isPrime(N)){
+            System.out.print("Yes");
+        }
+        else{
+            System.out.print("No");
+        }
+    }
+}
