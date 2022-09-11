ce71f377f2c2c31160c5547b83818f9011739610
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-09-11 12:44:58 +0530

    Create 5.areaOfCircle.java

---
+++ Basics/5.areaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int radius=sc.nextInt();
+		double area=Math.PI*radius*radius;
+		System.out.print(area);
+	}
+}
