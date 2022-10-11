3d8a279075544ccf9054b58f344ae51d8856928a
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-10-11 14:49:56 +0530

    Update 2389.LongestSubsequenceWithLimitedSum.java

--- Contest Problems/Weekly Contest 308/2389.LongestSubsequenceWithLimitedSum.java
+++ Contest Problems/Weekly Contest 308/2389.LongestSubsequenceWithLimitedSum.java
@@ -4,13 +4,11 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<queries.length;i++){
             int right=0,sum=0;
-            int max=0;
             while(right<nums.length && sum+nums[right]<=queries[i]){
                 sum+=nums[right];
-                max=right+1;
                 right++;
             }
-            result[i]=max;
+            result[i]=right;
         }
         return result;
     }
