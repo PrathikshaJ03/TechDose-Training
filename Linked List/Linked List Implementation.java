b54a2403cf7d52caa9a914a1de244bdd1d4ae7d3
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-10-25 19:02:44 +0530

    Update Linked List Implementation.java

--- Linked List/Linked List Implementation.java
+++ Linked List/Linked List Implementation.java
@@ -1,43 +1,91 @@
-import java.util.*;
 class Node{
     int data;
     Node next;
+    Node(int data){
+        this.data=data;
+    }
 }
-class Main{    
-    public static Node append(Node head,int data){
-        Node newNode=new Node();
-        newNode.data=data;
+
+class LinkedList{
+    Node head=null;
+    void append(int data){
+        Node newNode=new Node(data);
         newNode.next=null;
         if(head==null){
-            return newNode;
+            head=newNode;
+            return;
         }
         Node temp=head;
         while(temp.next!=null){
             temp=temp.next;
         }
         temp.next=newNode;
-        return head;
     }
-    
-    public static void print(Node head){
+    void insertAtBegin(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            newNode.next=null;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    void insertAtMiddle(int pos,int data){
+        Node newNode=new Node(data);
+        Node temp=head,prev=head;
+        for(int i=0;i<pos-1;i++){
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=newNode;
+        newNode.next=temp;
+    }
+    void removeAtFirst(){
+        if(head!=null)
+        head=head.next;
+    }
+    void removeAtEnd(){
+        Node temp=head;
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next=null;
+    }
+    void removeAtMiddle(int pos){
+        Node temp=head,prev=head;
+        for(int i=0;i<pos-1;i++){
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=temp.next;
+    }
+    void print(){
         Node temp=head;
         while(temp!=null){
             System.out.print(temp.data+" ");
             temp=temp.next;
         }
-        System.out.print("null");
+        System.out.println();
     }
-    
-    
-	public static void main(String[] args) {
-	    Node head=null;
-		Scanner sc=new Scanner(System.in);
-		int N=sc.nextInt();
-		int val;
-		for(int i=0;i<N;i++){
-		    val=sc.nextInt();
-		    head=append(head,val);
-		}
-		print(head);
-	}
+}
+
+class Main{
+    public static void main(String args[]){
+        LinkedList list=new LinkedList();
+        list.append(30);
+        list.append(20);
+        list.print();
+        list.insertAtBegin(60);
+        list.insertAtBegin(80);
+        list.print();
+        list.insertAtMiddle(3,70);
+        list.print();
+        list.removeAtFirst();
+        list.print();
+        list.removeAtEnd();
+        list.print();
+        list.removeAtMiddle(2);
+        list.print();
+    }
 }
