95c0c0beae51193a2aa885d36c9d895dde15aff1
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-11-02 10:29:57 +0530

    Create Sum of N natural numbers.java

---
+++ Recursion/Sum of N natural numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static int sumOfNaturalNos(int N){
+        if(N==1){
+            return 1;
+        }
+        return N+sumOfNaturalNos(N-1);
+    }
+    
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		System.out.print(sumOfNaturalNos(N));
+	}
+}
