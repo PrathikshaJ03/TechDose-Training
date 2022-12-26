c9f81751415bdfb51920ee02aba93e0df7c5ab23
PrathikshaJ03 <108274917+PrathikshaJ03@users.noreply.github.com>
2022-12-26 16:06:02 +0530

    Create Kruskal's Algorithm.java

---
+++ Graph/Kruskal's Algorithm.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class Main{
+    
+    static int parent[];
+    static int rank[];
+    
+    public static int find(int n){
+        if(parent[n]==-1){
+            return n;
+        }
+        parent[n]=find(parent[n]);
+        return parent[n];
+    }
+    
+    public static boolean union(int from,int to){
+        from=find(from);
+        to=find(to);
+        if(from==to){
+            return true;
+        }
+        if(rank[from]>rank[to]){
+            parent[to]=from;
+        }
+        else if(rank[to]>rank[from]){
+            parent[from]=to;
+        }
+        else{
+            parent[from]=to;
+            rank[to]+=1;
+        }
+        return false;
+    }
+    
+    public static void findMST(int n,int graph[][]){
+        
+        parent=new int[n];
+        rank=new int[n];
+        Arrays.sort(graph,(a,b)->a[2]-b[2]);
+        Arrays.fill(parent,-1);
+        
+        int i=1,j=0;
+        List<int[]> res=new ArrayList<>();
+        
+        while(i<n && j<graph.length){
+            if(union(graph[j][0],graph[j][1])){
+                j++;
+                continue;
+            }
+            res.add(new int[]{graph[j][0],graph[j][1]});
+            i++;
+            j++;
+        }
+        
+        for(i=0;i<res.size();i++){
+            System.out.println(res.get(i)[0]+" "+res.get(i)[1]);
+        }
+    }
+    
+	public static void main(String[] args) {
+		int n=4;
+		int graph[][]={{0,1,3},{1,2,2},{0,2,1},{2,3,5},{1,3,4}};
+		findMST(n,graph);
+	}
+}
